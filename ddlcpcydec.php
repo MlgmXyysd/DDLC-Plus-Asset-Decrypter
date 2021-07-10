4c8e767420519c4de967d08a2995f619776da58f
Jim Wu <mlgmxyysd@meowcat.org>
2021-07-10 22:35:56 +0800

    ddlcpcydec: Apache License 2.0

--- ddlcpcydec.php
+++ ddlcpcydec.php
@@ -1,14 +1,32 @@
-<?php
+<?php 
+/**
+ * 
+ *    Copyright (C) 2013-2021 MeowCat Studio Powered by MlgmXyysd All Rights Reserved.
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        https://github.com/MlgmXyysd/DDLC-Plus-Asset-Decrypter/blob/master/LICENSE
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *  
+ */
+
 /**
  * Doki Doki Literature Club Plus Asset Decrypter
  * 
- * Used to decrypt encrypted Streaming Asset Bundle files (*.cy) in DDLC-Plus.
- * 
- * Apache License v2.0
  * https://github.com/MlgmXyysd/DDLC-Plus-Asset-Decrypter
+ * 
+ * Used to decrypt encrypted Streaming Asset Bundle files (*.cy) in DDLC-Plus.
  * 
  * @author MlgmXyysd
  * @version 1.1
+ * 
  */
 
 $ver = "1.1";
